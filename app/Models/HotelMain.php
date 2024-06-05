9b76dcfa6bd8e0d27fc261738ed1bcb17153f0e6
Spb-Webmaster <spb-webmaster@mail.ru>
2024-06-05 10:39:25 +0300

    05June-3

---
+++ app/Models/HotelMain.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HotelMain extends Model
+{
+  protected $table = 'hotel_mains';
+  protected $fillable = [
+      'title',
+      'slug',
+      'price',
+      'img',
+      'star',
+      'country',
+      'meal',
+
+      'placement',
+      'mealrussian',
+      '',
+  ];
+}
