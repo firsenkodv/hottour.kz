8d63d8b5b69e199d5095d047de3e106f5df4f344
Spb-Webmaster <spb-webmaster@mail.ru>
2024-09-17 16:50:46 +0300

    17aug-10

--- resources/views/pages/find-tour.blade.php
+++ resources/views/pages/find-tour.blade.php
@@ -4,8 +4,10 @@
 @section('keywords', ($seo_keywords)??null)
 @section('content')
     <main class="page_search__wrapper">
+        <div class="block">
         <div class="pad_20 tv-search-form tv-moduleid-9960271"></div>
         <script type="text/javascript" src="//tourvisor.ru/module/init.js"></script>
+        </div>
     </main>
 @endsection
 
