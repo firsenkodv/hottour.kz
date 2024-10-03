cb15d80eb4cdcdb93e5c27497835357a15b863ac
Spb-Webmaster <spb-webmaster@mail.ru>
2024-10-03 12:13:40 +0300

    03okt-3

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,13 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TCPGTX2');</script>
-    <!-- End Google Tag Manager -->
+    {{ config('google.google_tag.head') }}
     <meta name="csrf-token" content="{{{ csrf_token() }}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite([
@@ -26,10 +20,7 @@
     <meta name="keywords" content="@yield('keywords',  config('seo.seo.keywords'))"/>
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCPGTX2"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+{{ config('google.google_tag.body') }}
     <div class="content_ {{ route_name() }}">
         @include('html.mobile.top')
         <x-message.message/>
